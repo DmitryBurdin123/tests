cc1d84cc96ddcfa03175d0122f0645e520566025
DmitryBurdin123 <101475609+DmitryBurdin123@users.noreply.github.com>
2024-02-27 19:17:43 +0300

    Update task2.java

--- task2/task2.java
+++ task2/task2.java
@@ -4,39 +4,37 @@ import java.util.Scanner;
 
 public class task2 {
     public static void main(String[] args) {
-        double xCenter;
-        double yCenter;
-        double radius;
-        double xPoint;
-        double yPoint;
+        if (args.length != 2) {
+            System.out.println("Требуется ввести 2 аргумента");
+            return;
+        }
+
+        String circleFilename = args[0];
+        String pointsFilename = args[1];
 
         try {
+            File circleFile = new File(circleFilename);
+            Scanner circleScanner = new Scanner(circleFile);
+            double xCenter = circleScanner.nextDouble();
+            double yCenter = circleScanner.nextDouble();
+            double radius = circleScanner.nextDouble();
+            circleScanner.close();
 
-            File file1 = new File("C:\\Program Files\\JetBrains\\file1.txt");
-            Scanner scanner1 = new Scanner(file1);
-            xCenter = scanner1.nextDouble();
-            yCenter = scanner1.nextDouble();
-            radius = scanner1.nextDouble();
-            scanner1.close();
-
-
-            File file2 = new File("C:\\Program Files\\JetBrains\\file2.txt");
-            Scanner scanner2 = new Scanner(file2);
-            xPoint = scanner2.nextDouble();
-            yPoint = scanner2.nextDouble();
-            scanner2.close();
-
-
-            double distance = Math.sqrt(Math.pow((xCenter - xPoint), 2) + Math.pow((yCenter - yPoint), 2));
-
-
-            if (distance < radius) {
-                System.out.println(1);
-            } else if (distance == radius) {
-                System.out.println(0);
-            } else {
-                System.out.println(2);
+            File pointsFile = new File(pointsFilename);
+            Scanner pointsScanner = new Scanner(pointsFile);
+            while (pointsScanner.hasNextDouble()) {
+                double xPoint = pointsScanner.nextDouble();
+                double yPoint = pointsScanner.nextDouble();
+                double distance = Math.sqrt(Math.pow((xCenter - xPoint), 2) + Math.pow((yCenter - yPoint), 2));
+                if (distance < radius) {
+                    System.out.println(1);
+                } else if (distance == radius) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(2);
+                }
             }
+            pointsScanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
             e.printStackTrace();
