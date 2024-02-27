42a53762d92a7cf7a6c876ee08bb1737715163b5
DmitryBurdin123 <101475609+DmitryBurdin123@users.noreply.github.com>
2024-02-27 22:06:31 +0300

    Update task1.java

--- task1/task1.java
+++ task1/task1.java
@@ -7,13 +7,20 @@ public class task1 {
         int n = Integer.parseInt(args[0]);
         int m = Integer.parseInt(args[1]);
 
-        int[] arr = new int[n];
-        Arrays.setAll(arr, i -> ++i);
+        if (n >=1 && m>=1){
+            if(n>1 && m==1){
+                System.out.println("Введите другие значения");
+                return;
+            }
 
-        int current = 0;
-        do {
-            System.out.print(arr[current]);
-            current = (current + m - 1) % n;
-        } while (current != 0);
+            int[] arr = new int[n];
+            Arrays.setAll(arr, i -> ++i);
+
+            int current = 0;
+            do {
+                System.out.print(arr[current]);
+                current = (current + m - 1) % n;
+            } while (current != 0);
+        }
     }
 }
